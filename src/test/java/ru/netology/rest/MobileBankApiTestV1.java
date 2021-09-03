8f7ad5b18644dce5b91569c65247e93c6524fc39
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-09-03 16:34:01 +0300

    Update MobileBankApiTestV1.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -18,9 +18,9 @@ class MobileBankApiTestV1 {
                 .then()
                   .statusCode(200)
                 // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath.
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+//                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         
-;
+// ;
     }
 }
    
