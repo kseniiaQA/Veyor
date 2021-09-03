33c5a660bdeb8bee901100d1148895bb733ebaef
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-09-03 16:20:58 +0300

    Update MobileBankApiTestV4.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,4 +1,3 @@
-/*
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
@@ -9,17 +8,17 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
-                .when()
-                .get("/demo/accounts")
-                // Проверки
-                .then()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
     }
-}*/
+}
